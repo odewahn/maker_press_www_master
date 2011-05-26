76ff6c91b80ced4114964b2bbe33eb98f2eb76ca
root <root@ip-10-194-102-120.ec2.internal>
2011-05-26 20:31:18 +0000

    fixed height of svgdraw

--- html/content/content_svgedit.php
+++ html/content/content_svgedit.php
@@ -4,5 +4,5 @@
 
 
 <div style="padding: 10px">
-   <iframe src="<?php echo $config["svgedit"]; ?>" width="100%" height="600" frameborder="1" marginheight="0" marginwidth="0">Loading...</iframe>
+   <iframe src="<?php echo $config["svgedit"]; ?>" width="100%" height="800" frameborder="1" marginheight="0" marginwidth="0">Loading...</iframe>
 </div>
